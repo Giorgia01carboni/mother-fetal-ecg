b3895a5a9fc5ab69507de9bf4a39f11f79b0c513
giorgia01carboni <giorgia.carboni@gmail.com>
2025-01-29 19:03:25 +0100

    Point 6 done

--- mother_fetal_ecg_separation.py
+++ mother_fetal_ecg_separation.py
@@ -1,10 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# abdominal_ecg -> 5 signals, first one is scalp on the fetus ecg
+# and the other 4 are abdominal ecg on the mother
+# fetal_beat_positions -> positions of the fetal beats (used as a reference)
+# sampling rate = 1000Hz
 pos = np.loadtxt('fetal_beat_positions.csv', delimiter=',')
 print(pos.shape)
 Y = np.loadtxt('abdominal_ecg.csv', delimiter=',')
 scalpECG = Y[:, 0]
+# aY -> abdominal ecgs
 aY = Y[:, 1:5]
 aY = np.matrix.transpose(aY)
 
@@ -24,9 +29,14 @@ plt.show()
 print()
 
 # Principal Component Analysis
-covariance = np.cov(aY)
+covariance = np.cov(aY, rowvar=True)
 
 val, V = np.linalg.eig(covariance)
+
+# eigenvalues ordered in descending order
+idx = np.argsort(val)[::-1]
+val = val[idx]
+V = V[:, idx]
 
 # W -> unmixing matrix, which is the eigenvector matrix V transposed.
 W = np.matrix.transpose(V)
@@ -41,3 +51,13 @@ for j in range(4):
   ax[j].set_title(f"PCA of source {j+1}")
 plt.tight_layout()
 plt.show()
+
+# Variance of each estimated source
+variances = np.var(Xpca, axis=1)
+print(f"Variances: {variances}")
+print(f"Eigenvalues: {val}")
+
+normalized_val = val / np.sum(val)
+tot_variance = np.cumsum(normalized_val)
+num_components = np.argmax(tot_variance >= 0.90) + 1
+print("Number of components which cover at least 90% of the overall variance: ", num_components)
